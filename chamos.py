c0dbaf39c32b5060f4286db7f4db00dcc768ee81
joshuaSmith2021 <joshuasmith6556@gmail.com>
2019-09-29 08:12:12 -0700

    Save before working on socket reader

---
+++ chamos.py
@@ -0,0 +1,32 @@
+import discord
+import tools
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!ip'):
+            await message.channel.send('The *survival* server\'s ip is icecraft.hosthorde.net. This IP isn\'t going to change anytime soon! The *PVP* server\'s IP is currently {0}. Heads up, this IP tends to change!'.format(tools.get_ip()))
+        elif message.content.startswith('pls corgi'):
+            gif = await tools.get_gif('corgi')
+            await message.channel.send(gif)
+            await message.channel.send('https://www.danasilver.org/giphymessages/PoweredBy_Horizontal_Light-Backgrounds.gif')
+
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
+            await guild.system_channel.send(to_send)
+            # await member.add_roles(tools.Memeber)
+
+
+client = MyClient()
+client.run('NjI1NTAxMDcyMzk5NTMyMDQy.XYmeEg.FYz9nmX0CNxJa2n3wa7e9Hoq6Sw')
---
+++ tools.py
@@ -0,0 +1,28 @@
+import requests
+import re
+import random
+
+import giphy_client as gif
+from giphy_client.rest import ApiException
+
+giphy = gif.DefaultApi()
+
+def get_ip():
+    page = requests.get('https://secret-forest-58202.herokuapp.com/').text
+    return re.findall(r'0\.tcp\.ngrok\.io:[0-9]{5}', page)[0]
+
+
+class Memeber():
+    id = ''
+
+
+async def get_gif(query):
+    try:
+        giphy_token = 'tiF0yt05LjvjDWtN3FcO0UY70NWXC9DL'
+        response = giphy.gifs_search_get(giphy_token, query, limit=20, rating='g')
+        gifs = list(response.data)
+        random.shuffle(gifs)
+        return gifs[0].url
+    except ApiException as e:
+        return 'Uh oh gamers, I couldn\'t grab the {0} gif...'.format(query)
+
